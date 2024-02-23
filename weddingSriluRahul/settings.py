8b295e34f4ad14822d818eadfbde0e9a4cbcb1df
rahulsrilalitha <rahulsrilalitha@gmail.com>
2024-02-23 18:15:33 +0530

    Added RDS DB settings

--- weddingSriluRahul/settings.py
+++ weddingSriluRahul/settings.py
@@ -128,9 +128,12 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# SMTP configuration
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'rahul.varma.nagidi@gmail.com'
-EMAIL_HOST_PASSWORD = 'ochmzpqerytrxkir'
-EMAIL_USE_TLS = 'True'
+AWS_ACCESS_KEY_ID = 'AKIAQ3EGQ2ARZC24MKSD'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'rahulsrilalitha'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'ap-southeast-2'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL =  None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
